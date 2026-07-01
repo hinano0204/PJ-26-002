8805da9708dc26a49cb9cf2a6368df80fbf108a6
Sugimori.HN <nkc25.sugimori.hinano@gmail.com>
2026-07-01 14:55:39 +0900

    Add.触るな危険

--- ProjectObake/Assets/Scripts/PlayerMove.cs
+++ ProjectObake/Assets/Scripts/PlayerMove.cs
@@ -3,7 +3,7 @@ using System.Runtime.CompilerServices;
 using UnityEngine;
 using Unity.Netcode;
 
-public class PlayerMove : MonoBehaviour
+public class PlayerMove : NetworkBehaviour
 {
     public float movespeed = 5f;
     public Rigidbody rb;
@@ -22,33 +22,31 @@ public class PlayerMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        /*
+        
         //自分のオブジェクトだけ入力を設定
         if (IsOwner)
         {
             // 移動入力を設定
-        */
+        
 
         float x = Input.GetAxisRaw("Horizontal");   //左右　移動キー
-        float y = Input.GetAxisRaw("Vertical");
+        float y = Input.GetAxisRaw("Vertical");     //上下キー
 
         Vector3 move = new Vector3(x, y, 0);
 
         rb.linearVelocity = move * movespeed;
 
-        //SetMoveInputServerRpc(
-        //        Input.GetAxisRaw("Horizontal"),
-        //        Input.GetAxisRaw("Vertical"));
-        /*
+            //SetMoveInputServerRpc(
+            //        Input.GetAxisRaw("Horizontal"),
+            //        Input.GetAxisRaw("Vertical"));
+            Debug.Log("入力中");
         }
 
         //ホストの場合
         if (IsHost)
         {
-            HostUpdate();
+            //HostUpdate();
         }
-        */
 
-        
     }
 }
